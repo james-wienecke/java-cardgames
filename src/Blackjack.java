31a1f26a0aa15614aaaf9b3562947026c242a1c6
James Wienecke <james.a.wienecke@gmail.com>
2022-01-10 15:15:34 -0600

    Implemented a secondary constructor that allows an arbitrary shoe size to be set.
    A dealing shoe or dealer's shoe is a gaming device, mainly used in casinos, to hold multiple decks of playing cards. The shoe allows for more games to be played by reducing the time between shuffles and less chance of dealer cheating. In some games, such as blackjack (where card counting is a possibility), using multiple decks of cards can increase the house edge.

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,8 +1,15 @@
 public class Blackjack {
 
     public static void main(String[] args) {
-        Deck deck = new Deck(1);
-        for (Card card : deck.getCards()) {
+        Deck deck1 = new Deck();
+        for (Card card : deck1.getCards()) {
+            System.out.println(String.format("%c%c", card.getSuitChar(), card.getValueAsChar()));
+        }
+
+        System.out.println("Deck 2 -- 4 deck shoe");
+
+        Deck deck2 = new Deck(4);
+        for (Card card : deck2.getCards()) {
             System.out.println(String.format("%c%c", card.getSuitChar(), card.getValueAsChar()));
         }
     }
--- src/Deck.java
+++ src/Deck.java
@@ -3,9 +3,16 @@ import java.util.LinkedList;
 public class Deck {
     private LinkedList<Card> cards;
 
-    public Deck(int shoeSize) {
+    public Deck() {
         this.cards = new LinkedList<>();
         fillDeck();
+    }
+
+    public Deck(int shoeSize) {
+        this.cards = new LinkedList<>();
+        for (int i = 0; i < shoeSize; i++) {
+            fillDeck();
+        }
     }
 
     public void fillDeck() {
